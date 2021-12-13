e07e3caf37beb1f9de905afbdca3452faeb97ac4
jain95garima <jain95garima@gmail.com>
2021-12-14 01:31:15 +0530

    Added a new sum function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
-print("adding all the data processing code")
+print("adding all the data processing code related to fraud detection.")
+
+a = 5
+b = 8
+def sum( a,b):
+    c = a+b
+    return a
+
+print("Adding a new sum fucntion.")
