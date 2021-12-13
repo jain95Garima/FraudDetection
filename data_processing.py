89e8883aee6f51e8db105eb252cc824cb4039ef2
jain95garima <jain95garima@gmail.com>
2021-12-14 01:34:01 +0530

    Revert "Added a new sum function"
    This reverts commit e07e3caf37beb1f9de905afbdca3452faeb97ac4.

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
-print("adding all the data processing code related to fraud detection.")
-
-a = 5
-b = 8
-def sum( a,b):
-    c = a+b
-    return a
-
-print("Adding a new sum fucntion.")
+print("adding all the data processing code")
