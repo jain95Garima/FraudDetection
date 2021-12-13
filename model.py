8900841e35a5526630fa06032f39bc82659ee149
jain95garima <jain95garima@gmail.com>
2021-12-14 01:58:20 +0530

    updated ML model code.

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
 print("adding all codes related to Ml model for fraud detection.")
+
+def model1():
+    print("add codel for model 1.")
+    
+def model2():
+    print("add codel for model 2")
+    
